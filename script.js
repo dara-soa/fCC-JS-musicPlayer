895a7a0275c138d0a83560536adc2353e8ae6786
Daraujo-Soares <144711354+dara-soa@users.noreply.github.com>
2024-05-14 22:44:47 -0300

    # created renderSongs array

--- script.js
+++ script.js
@@ -79,9 +79,13 @@ const allSongs = [
 ];
 
 const audio = new Audio();
+
 let userData = {
   songs: [...allSongs],
   currentSong: null,
   songCurrentTime: 0,
 };
 
+const renderSongs = (array) => {
+  const songsHTML = array.map();
+};
